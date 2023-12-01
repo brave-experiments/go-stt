56316112a96c0527f8fc454365207c290146e849
boocmp <boocmp@yandex.ru>
2023-12-01 19:40:13 +0700

    faster whisper

--- bentoml/runners/audio_transcriber.py
+++ bentoml/runners/audio_transcriber.py
@@ -1,20 +1,24 @@
 import bentoml
-import whisper
 import torch
-import numpy as np
+from faster_whisper import WhisperModel
+from bentoml.io import JSON
 
 class AudioTranscriber(bentoml.Runnable):
     SUPPORTED_RESOURCES = ("nvidia.com/gpu", "cpu")
     SUPPORTS_CPU_MULTI_THREADING = True
 
     def __init__(self):
-        self.model = whisper.load_model("base")
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        compute_type = "float16" if torch.cuda.is_available() else "int8"
+        model = "base_en"
+        self.model = WhisperModel(model, device=device, compute_type=compute_type)
 
     @bentoml.Runnable.method(batchable=False)
-    def transcribe_audio(self, audio):
-        data = np.frombuffer(audio, np.float32).astype(np.float32)
-        data = whisper.pad_or_trim(data)
-        transcription = self.model.transcribe(data)
-        return transcription['text']
+    def transcribe_audio(self, audio):        
+        segments = self.model.transcribe(audio, vad_filter=True, vad_parameters=dict(min_silence_duration_ms=500))
+
+        text = ""
+        for s in segments:
+            text = text + " "  + s.text
+
+        return { "text" : text }
