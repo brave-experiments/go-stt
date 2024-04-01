78e5f2205bc97151ed6e54445b761c81918d2dbb
boocmp <boocmp@yandex.ru>
2024-04-01 18:10:14 +0700

    process-audio-data

--- src/service.py
+++ src/service.py
@@ -21,6 +21,6 @@ async def process_audio(input_file: io.BytesIO):
     return transcript
 
 @svc.api(input=File(), output=JSON())
-async def process_audio(input_file: io.BytesIO):
-    transcript = await runner_audio_transcriber.transcribe_audio.async_run(input_file.read())
+async def process_audio_data(input_file: io.BytesIO):
+    transcript = await runner_audio_transcriber.transcribe_audio_data.async_run(input_file.read())
     return transcript
