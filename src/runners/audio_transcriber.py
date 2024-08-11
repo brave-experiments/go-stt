bf59e94f9bf172a2bb988f0d7ec8263fe6cd172b
boocmp <boocmp@yandex.ru>
2024-08-11 16:26:01 +0700

    20 workers.

--- src/runners/audio_transcriber.py
+++ src/runners/audio_transcriber.py
@@ -15,7 +15,7 @@ class AudioTranscriber(bentoml.Runnable):
 
         print(device, " ", compute_type)
 
-        model = "large-v3"
+        model = "medium"
         self.model = WhisperModel(model, device=device, compute_type=compute_type)
 
     @bentoml.Runnable.method(batchable=False)
